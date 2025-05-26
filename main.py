09d16593b02aa8bc359dd5c381ba89cbbc893235
amithaisuru <rkamithaisuru@gmail.com>
2025-05-26 09:55:09 +0530

    set title and icon

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import pygame
 
-pygame.init()
 WIDTH, HEIGHT = 800, 600
 
+pygame.init()
+
+#create screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+#title and icon
+pygame.display.set_caption("Space Invadors")
+icon = pygame.image.load('assets/icon.png')
+pygame.display.set_icon(icon)
 
 #game loop
 running = True
